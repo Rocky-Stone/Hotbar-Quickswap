b3b6c30264760abfacbce3b065d69e16b9102e5b
Rocky-Stone <maxabro@yahoo.com>
2025-08-09 21:46:17 -0700

    initial commit, working state

---
+++ src/main/java/net/rockystone/HotbarQuickswap.java
@@ -0,0 +1,63 @@
+package net.rockystone;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.screen.slot.SlotActionType;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Environment(EnvType.CLIENT)
+public class HotbarQuickswap implements ClientModInitializer {
+
+	private static KeyBinding quickswapKeyBind;
+
+	public static final String MOD_ID = "hotbar-quickswap";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("onInitializeClient() was called");
+		setKeyBindings();
+		registerEvents();
+	}
+
+
+
+	public void setKeyBindings() {
+		LOGGER.info("setKeyBindings() was called");
+		quickswapKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.hotbar-quickswap.config", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, "category.hotbar-quickswap.keybindings"));
+	}
+
+	public void registerEvents() {
+		LOGGER.info("registerEvents() was called");
+		registerEndClientTickEvent();
+	}
+
+	public void hotbarQuickswap(MinecraftClient client) {
+		if (client.player == null) {
+			return;
+		}
+		int syncID = client.player.playerScreenHandler.syncId;
+		int currentlySelectedSlot = client.player.getInventory().getSelectedSlot();
+		int aboveSlot = currentlySelectedSlot + 27;
+		client.interactionManager.clickSlot(syncID, aboveSlot, currentlySelectedSlot, SlotActionType.SWAP, client.player);
+	}
+
+	public void registerEndClientTickEvent() {
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+
+			while (quickswapKeyBind.wasPressed()) {
+				hotbarQuickswap(client);
+			}
+
+		});
+	}
+}
